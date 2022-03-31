16665c8eef9282abfb82a783a5481307c64ef6ee
chriscoded <amaemechris@gmail.com>
2022-03-31 02:02:36 -1200

    Add project files.

---
+++ SplashShoppingCart/Infrastructure/SplashShoppingCartContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SplashShoppingCart.Infrastructure
+{
+    public class SplashShoppingCartContext : DbContext
+    {
+        public SplashShoppingCartContext(DbContextOptions<SplashShoppingCartContext> options)
+            : base(options)
+        {
+        }
+    }
+}
