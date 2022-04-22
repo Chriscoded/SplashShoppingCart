60babba9a8b446336cd9bb023109154260bd46cf
chriscoded <amaemechris@gmail.com>
2022-04-21 18:23:13 -1200

    Custom error handler for non development environment

---
+++ SplashShoppingCart/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SplashShoppingCart.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "That Page does not exist";
+                    break;
+                default:
+                    ViewBag.ErrorMessage = "Problem occured";
+                    break;
+            }
+            return View("NotFound");
+        }
+    }
+}
--- SplashShoppingCart/Startup.cs
+++ SplashShoppingCart/Startup.cs
@@ -62,10 +62,14 @@ namespace SplashShoppingCart
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                ////app.UseStatusCodePagesWithReExecute("/Error/{0}");
+                //app.UseStatusCodePagesWithRedirects("/Error/{0}");
+
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                //app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -83,6 +87,11 @@ namespace SplashShoppingCart
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                   "error",
+                   "error/{statusCode}",
+                   defaults: new {controller = "Error", action = "HttpStatusCodeHandler" }
+                 );
                 endpoints.MapControllerRoute(
                    name: "areas",
                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
